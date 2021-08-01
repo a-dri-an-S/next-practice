624ec566eb3a202960f80eefc02ff439322a07bd
ADD RIAN <adrian.salinas.1986@gmail.com>
2021-08-01 16:35:42 -0700

    created custom Document page

--- components/Layout.js
+++ components/Layout.js
@@ -11,9 +11,8 @@ export default ({ children, title }) => (
     <div className="root">
         <Head>
             <title>Next Portfolio</title>
-            <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/nprogress/0.2.0/nprogress.min.css"/>
         </Head>
-        
+
         <header>
             <Link href="/"><a>Home</a></Link>
             <Link href="/about"><a>About</a></Link>
