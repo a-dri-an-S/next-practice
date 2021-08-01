341474a0adcdb3f30d927342a4f09d947e0d1898
ADD RIAN <adrian.salinas.1986@gmail.com>
2021-08-01 15:15:20 -0700

    created hireme.js page

---
+++ pages/hireme.js
@@ -0,0 +1,10 @@
+const HireMe = () => (
+    <div>
+        <h1>Hire Me</h1>
+        <p>You can hire me at:  
+            <a href="mailto:adriansalinas.tech@gmail.com"> adriansalinas.tech@gmail.com</a>
+        </p>
+    </div>
+)
+
+export default HireMe
